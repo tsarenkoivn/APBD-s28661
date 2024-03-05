e2a7d2cdf972cb142253fa3995cb713867395489
tsarenkoivn <s28661@pjwstk.edu.pl>
2024-03-05 14:56:15 +0100

    My first commit

---
+++ FirstAPBDTask/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            if (int.TryParse(Console.ReadLine(), out int result)){
+                if (result > 0) {
+                    Console.WriteLine("The value is bigger than zero");
+                }
+                else if (result < 0) {
+                    Console.WriteLine("The value is less than zero");
+                }
+                else if(result == 0)
+                {
+                    Console.WriteLine("You have entered zero");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input plese enter an integer");
+                }
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+}
