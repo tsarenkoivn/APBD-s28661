5fde4ffdc77ac015a140c239320dfe24aa31bc72
s28672 <s28672@pjwstk.edu.pl>
2024-04-15 21:16:21 +0200

    implemented controlers for visit

--- Task4/Controllers/VisitsController.cs
+++ Task4/Controllers/VisitsController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Task4.models;
 
 namespace Task4.Controllers
@@ -8,15 +11,46 @@ namespace Task4.Controllers
     public class VisitsController : ControllerBase
     {
         // List of visits
+        public static List<Visit> Visits = new List<Visit>();
 
         //Retrieve all visits
         [HttpGet]
+        public IActionResult Get => Ok(Visits);
 
         //Retrieve visists with specific animal by the Id
         [HttpGet("{animalId}")]
+        public IActionResult Get(Guid id)
+        {
+            var visit = Visits.FirstOrDefault(a => a.animalId == animalID);
+            if (visit == null) return NotFound();
+            return Ok(Visit);
+        }
 
         //Add a new visit
         [HttpPost]
+        public IActionResult Post(Visit visit)
+        {
+            Visits.Add(visit);
+            return CreatedAtAction(nameof(Get), new { id == visit.id }, visit)
+        }
 
+        //Edit visit
+        [HttpPut]
+        public IActionResult Put(Guid id, Visit updatedVisit) {
+            var index = animals.FindIndex(a => a.Id == id);
+            if (index == -1) return NotFound();
+            animals[index] = updatedAnimal;
+            return NoContent();
+        }
+
+        //Delte visit
+        [HttpDelete]
+        public IActionResult Delete(Guid id)
+        {
+            var index = animals.FindIndex(a => a.Id == id);
+            if (index == -1) return NotFound();
+            animals.RemoveAt(index);
+            return NoContent();
+        }
     }
 }
--- Task4/models/Visit.cs
+++ Task4/models/Visit.cs
@@ -1,7 +1,14 @@
-﻿namespace Task4.models
+﻿using System;
+
+namespace Task4.models
 {
     public class Visit
     {
         // id of visists [Guid], datesofvisits [DateTime], AnimalId, Description, Price
+        public Guid id { get; set; } = Guid.NewGuid();
+        public DateTime dateofvisits { get; set; }
+        public int animalId { get; set; }
+        public string descriprtion { get; set; }
+        public float price { get; set; }
     }
 }
