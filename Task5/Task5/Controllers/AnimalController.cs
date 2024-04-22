47dc2f4d3466c91a27a47e7a62481f2db272b8a8
tsarenkoivn <s28661@pjwstk.edu.pl>
2024-04-23 01:31:25 +0200

    Completed the API for all requests and configured connection string to db

---
+++ Task5/Task5/Controllers/AnimalController.cs
@@ -0,0 +1,128 @@
+﻿using Microsoft.AspNetCore.Authorization.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using Task5.Models;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AnimalsController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public AnimalsController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    private List<Dictionary<string, object>> ConvertDataTableToList(DataTable dt)
+    {
+        var columns = dt.Columns.Cast<DataColumn>();
+        return dt.Rows.Cast<DataRow>()
+            .Select(row => columns.ToDictionary(column => column.ColumnName, column => row[column])).ToList();
+    }
+
+    // GET: api/animals
+    [HttpGet]
+    public IActionResult GetAnimals(string orderBy = "name")
+    {
+        string query = $"SELECT * FROM Animals ORDER BY {orderBy}";
+        DataTable table = new DataTable();
+        string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+        using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+        {
+            myCon.Open();
+            using (SqlCommand myCommand = new SqlCommand(query, myCon))
+            {
+                using (SqlDataReader myReader = myCommand.ExecuteReader())
+                {
+                    table.Load(myReader);
+                }
+            }
+            myCon.Close();
+        }
+
+        var list = ConvertDataTableToList(table);
+        return Ok(list);
+    }
+
+    // POST: api/animals
+    [HttpPost]
+    public IActionResult PostAnimal(Animal animal)
+    {
+        string query = @"
+           INSERT INTO Animals (Name, Description, Category, Area) 
+           VALUES (@Name, @Description, @Category, @Area)";
+        DataTable table = new DataTable();
+        string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+        SqlDataReader myReader;
+        using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+        {
+            myCon.Open();
+            using (SqlCommand myCommand = new SqlCommand(query, myCon))
+            {
+                myCommand.Parameters.AddWithValue("@Name", animal.Name);
+                myCommand.Parameters.AddWithValue("@Description", animal.Description);
+                myCommand.Parameters.AddWithValue("@Category", animal.Category);
+                myCommand.Parameters.AddWithValue("@Area", animal.Area);
+                myReader = myCommand.ExecuteReader();
+                table.Load(myReader);
+                myReader.Close();
+                myCon.Close();
+            }
+        }
+        return new JsonResult("Added Successfully");
+    }
+
+    [HttpDelete]
+    public IActionResult deleteAnimal(Animal animal)
+    {
+        string query = @"
+            DELETE FROM Animals
+            WHERE IdAnimal = @IdAnimal
+        ";
+        DataTable table = new DataTable();
+        string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+        SqlDataReader myReader;
+        using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+        {
+            myCon.Open();
+            using(SqlCommand myCommand = new SqlCommand(query, myCon))
+            {
+                myCommand.Parameters.AddWithValue("@IdAnimal", animal.IdAnimal);
+                myReader = myCommand.ExecuteReader();
+                table.Load(myReader);
+                myReader.Close();
+                myCon.Close();
+            }
+        }
+        return new JsonResult("Deleted Successfully");
+    }
+
+    [HttpPut]
+    public IActionResult updateAnimal(Animal animal, string attribute, string value)
+    {
+        string query = @"
+            UPDATE Animals
+            SET @attribute = '@value'
+            WHERE IdAnimal = @IdAnimal
+        ";
+        DataTable table = new DataTable();
+        string sqlDataSource = _configuration.GetConnectionString("DefaultConnection");
+        SqlDataReader myReader;
+        using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+        {
+            myCon.Open();
+            using (SqlCommand myCommand = new SqlCommand(query, myCon))
+            {
+                myCommand.Parameters.AddWithValue("@IdAnimal", animal.IdAnimal);
+                myCommand.Parameters.AddWithValue("@attribute", attribute);
+                myCommand.Parameters.AddWithValue("@value", value);
+                myReader = myCommand.ExecuteReader(); 
+                table.Load(myReader);
+                myReader.Close();
+                myCon.Close();
+            }
+        }
+        return new JsonResult("UPDATED Successfully");
+    }
+}
